ae22af59d6e95561d1c73d7e6c5b1a27965418d5
Emanuele Liardo <emanuele.liardo@codiceplastico.com>
2024-10-18 21:28:36 +0200

    use fetch instead of puppeteer (#121)

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,6 @@ declare module "investing-com-api" {
     period?: 'P1D' | 'P1W' | 'P1M' | 'P3M' | 'P6M' | 'P1Y' | 'P5Y' | 'MAX',
     interval?: 'PT1M' | 'PT5M' | 'PT15M' | 'PT30M' | 'PT1H' | 'PT5H' | 'P1D' | 'P1W' | 'P1M',
     pointscount?: 60 | 70 | 120,
-    pptrLaunchOptions?: any,
   ): Promise<{
     date: number,
     value: number,
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const puppeteer = require('puppeteer');
 const { mapping } = require('./mapping');
-const { getJsonContent, mapResponse } = require('./functions');
+const { mapResponse } = require('./functions');
 
 const validPeriod = ['P1D', 'P1W', 'P1M', 'P3M', 'P6M', 'P1Y', 'P5Y', 'MAX'];
 const validInterval = ['PT1M', 'PT5M', 'PT15M', 'PT30M', 'PT1H', 'PT5H', 'P1D', 'P1W', 'P1M'];
@@ -36,19 +35,23 @@ function checkParams(input, period, interval, pointscount) {
  * @param {string} interval Interval between results.
  *                          Valid values: PT1M, PT5M, PT15M, PT30M, PT1H, PT5H, P1D, P1W, P1M
  * @param {number} pointscount Number of results returned. Valid values: 60, 70, 120
- * @param {*} pptrLaunchOptions Puppeteer launch options, see https://pptr.dev/api/puppeteer.launchoptions
  * @return {Promise<Array>} An array of arrays with date (timestamp) and values (number) properties
  */
-async function callInvesting(pairId, period, interval, pointscount, pptrLaunchOptions) {
-  const browser = await puppeteer.launch(pptrLaunchOptions);
-  const page = await browser.newPage();
-  // eslint-disable-next-line max-len
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36');
-  // eslint-disable-next-line max-len
-  await page.goto(`https://api.investing.com/api/financialdata/${pairId}/historical/chart?period=${period}&interval=${interval}&pointscount=${pointscount}`);
-  const jsonContent = await getJsonContent(page);
-  await browser.close();
-  return jsonContent.data;
+async function callInvesting(pairId, period, interval, pointscount) {
+  const url = `https://api.investing.com/api/financialdata/${pairId}/historical/chart?period=${period}&interval=${interval}&pointscount=${pointscount}`
+
+  const response = await fetch(url, {
+    headers: new Headers({
+      "upgrade-insecure-requests": "1"
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const json = await response.json();
+  return json.data
 }
 
 /**
@@ -60,21 +63,19 @@ async function callInvesting(pairId, period, interval, pointscount, pptrLaunchOp
  *                          Valid values: PT1M, PT5M, PT15M, PT30M, PT1H, PT5H, P1D, P1W, P1M
  * @param {number} [pointscount] Number of results returned, but depends on period and interval too.
  *                             Valid values: 60, 70, 120
- * @param {*} [pptrLaunchOptions] Puppeteer launch options, see https://pptr.dev/api/puppeteer.launchoptions
  * @return {Promise<Array>} An array of objects with date (timestamp), value (number) and other (number) properties
  */
-async function investing(input, period = 'P1M', interval = 'P1D', pointscount = 120, pptrLaunchOptions) {
+async function investing(input, period = 'P1M', interval = 'P1D', pointscount = 120) {
   try {
     checkParams(input, period, interval, pointscount);
     const pairId = mapping[input]?.pairId || input;
-    const resInvesting = await callInvesting(pairId, period, interval, pointscount, pptrLaunchOptions);
+    const resInvesting = await callInvesting(pairId, period, interval, pointscount);
     const results = mapResponse(resInvesting);
     if (!results.length) {
       throw Error('Wrong input or pairId');
     }
     return results;
   } catch (err) {
-    console.error(err.message);
     if (err.response?.data?.['@errors']?.[0]) {
       console.error(err.response.data['@errors'][0]);
     }
