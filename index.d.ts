dcaf615e0bbd48db1c1b4112e30259ffb8c12ff9
Emanuele Liardo <emanuele.liardo@codiceplastico.com>
2024-10-20 22:19:19 +0200

    refactor ts definition

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,25 @@
 declare module "investing-com-api" {
+  interface InvestmentData {
+    date: number;
+    value: number;
+    price_open: number;
+    price_high: number;
+    price_low: number;
+    price_close: number;
+  }
+
   export function investing(
     input: string,
     period?: 'P1D' | 'P1W' | 'P1M' | 'P3M' | 'P6M' | 'P1Y' | 'P5Y' | 'MAX',
     interval?: 'PT1M' | 'PT5M' | 'PT15M' | 'PT30M' | 'PT1H' | 'PT5H' | 'P1D' | 'P1W' | 'P1M',
     pointscount?: 60 | 70 | 120,
     pptrLaunchOptions?: any,
-  ): Promise<{
-    date: number,
-    value: number,
-    price_open: number,
-    price_high: number,
-    price_low: number,
-    price_close: number,
-  }[]>;
+  ): Promise<InvestmentData[]>;
 
   export function getHistoricalData(
     input: string,
     resolution: '5' | '60' | 'D' | 'W' | 'M',
     from: Date,
     to: Date
-  ): Promise<{
-    date: number,
-    value: number,
-    price_open: number,
-    price_high: number,
-    price_low: number,
-    price_close: number,
-  }[]>;
+  ): Promise<InvestmentData[]>;
 }
