6e89a308a4ccafbe12e9ca73303eb8b1cbefd6e6
Manu <76907155+odrail@users.noreply.github.com>
2025-01-06 17:14:56 +0100

    Get Real Time Quotes API (#11)
    * Get Real Time Quotes API
    Fixes #6
    
    * RealTimeData documentation
    
    * doc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
+import { RealTimeData, PidInfo } from "./api/RealTimeData";
 import getHistoricalData from "./getHistoricalData";
+// export { PidInfo } from "./api/RealTimeData";
+
+const a : PidInfo = null
+
+console.log(a)
 
 export {
-    getHistoricalData
+    PidInfo
 }
+
+// export {
+//     // APIs
+//     getHistoricalData,
+//     RealTimeData,
+
+//     // Types
+//     PidInfo
+// }
+
+// const realTimeData = new RealTimeData();
+// realTimeData.on(RealTimeData.ON_OPEN, () => {
+//     realTimeData.subscribe([
+//         // 1057391, // 
+//         1061443
+//     ]);
+// });
+
+// realTimeData.on(RealTimeData.ON_DATA, (data: PidInfo) => {
+//     console.log(data);
+// })
+
+// realTimeData.on(RealTimeData.ON_CLOSE, () => {
+//     console.log('closed');
+// })
